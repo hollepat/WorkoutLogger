88c47e01a001a7c20afb0fd65c4e0dd0e7f47bef
Holler, Patrik <hollepat@fel.cvut.cz>
2023-05-26 14:48:25 +0200

    changed object to store exercise and fixed appearing of logged workouts

--- functions.js
+++ functions.js
@@ -1,4 +1,43 @@
-var exerciseData = {};
+
+class State {
+
+    #exercises;
+    #filter;
+
+    constructor (initExercises) {
+      this.#exercises = initExercises;
+    }
+
+    // arrow function declaration --> takt this from context, do not binf their own this
+    addExercise = function (exercise) {
+      this.#exercises.push(exercise);
+    }    
+
+    // define their own this depending on how they are called
+    setFilter (filter) {
+      this.#filter = filter;
+  }
+
+    getExercises = function (filter) {
+      this.#filter = filter;
+      return this.#exercises.filter((exercise) => {
+          if (this.#filter == 'all') {
+            return true;
+          } else {
+            return this.#filter == exercise.name;
+          }
+      });
+    }
+}
+
+
+const exerciseData = new State([
+  {name: "exercise1", date: new Date(), weight: 11},
+  {name: "exercise2", date: new Date(), weight: 12},
+  {name: "exercise3", date: new Date(), weight: 13},
+  {name: "exercise1", date: new Date(), weight: 15}
+])
+
 
 function logWorkout() {
   var exercise = document.getElementById("exercise").value;
@@ -6,29 +45,33 @@ function logWorkout() {
   var sets = document.getElementById("sets").value;
   var weight = document.getElementById("weight").value;
 
+  // new row
   var tableBody = document.getElementById("workoutTableBody");
   var newRow = tableBody.insertRow();
 
+  // new cell for name
   var exerciseCell = newRow.insertCell();
   exerciseCell.textContent = exercise;
 
+  // new cell for row
   var repsCell = newRow.insertCell();
   repsCell.textContent = reps;
 
+  // new cell for set
   var setsCell = newRow.insertCell();
   setsCell.textContent = sets;
 
+  // new cell for weight
   var weightCell = newRow.insertCell();
   weightCell.textContent = weight;
 
-  if (!exerciseData[exercise]) {
-    exerciseData[exercise] = [];
+  let newExercise = {
+      name: exercise,
+      date: new Date(),
+      weight: parseFloat(weight)
   }
 
-  exerciseData[exercise].push({
-    date: new Date(),
-    weight: parseFloat(weight),
-  });
+  exerciseData.addExercise(newExercise);
 
   updateChart();
 }
@@ -59,6 +102,7 @@ function createWorkout() {
   prCell.textContent = exercisePR;
 }
 
+// swithc current window
 function toggleWindow(currentWindow, nextWindow) {
   var current = document.getElementById(currentWindow);
   var next = document.getElementById(nextWindow);
@@ -68,10 +112,26 @@ function toggleWindow(currentWindow, nextWindow) {
 }
 
 function updateChart() {
-  var selectedExercise = document.getElementById("exerciseStats").value;
-  var data = exerciseData[selectedExercise] || [];
-  var chartContainer = document.getElementById("chartContainer");
 
+  // get window for charts
+  const window = document.getElementById("statsWindow");
+  
+  // get chosen exercise
+  const selectedExercise = document.getElementById("exerciseStats").value;
+  const data = exerciseData.getExercises(selectedExercise);
+
+  // remove old one if there is
+  let oldChartContainer = document.getElementById("chartContainer");
+  oldChartContainer.remove();
+
+  // create new canvas
+  let chartContainer = document.createElement("canvas");
+  chartContainer.id = "chartContainer";
+  chartContainer.style.width = "1vw"
+  chartContainer.style.height = "1.2vh"
+  window.appendChild(chartContainer);
+
+  // no exercise selected
   if (data.length === 0) {
     chartContainer.style.display = "none";
     return;
@@ -79,11 +139,14 @@ function updateChart() {
 
   chartContainer.style.display = "block";
 
-  var labels = data.map((item) => item.date.toDateString());
-  var weights = data.map((item) => item.weight);
+  // create data for chart
+  let labels = data.map((item) => item.date.toLocaleTimeString());
+  let weights = data.map((item) => item.weight);
 
-  var ctx = chartContainer.getContext("2d");
-
+  // get 2d drawing context for canvas
+  let ctx = chartContainer.getContext("2d");
+  
+  // create chart
   new Chart(ctx, {
     type: "line",
     data: {
