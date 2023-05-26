9f6cbdf46f0a245a49711fd4cc32ed197b23d6ba
Holler, Patrik <hollepat@fel.cvut.cz>
2023-05-26 22:02:25 +0200

    added options created using js

--- functions.js
+++ functions.js
@@ -1,134 +1,167 @@
 
-class State {
+const exercises = [
+    "Benchpress",
+    "Deadlift",
+    "Squat",
+    "Romanian deadlift",
+    "Bulgraina split squat",
+    "Biceps curls",
+    "Hammer curls"
+];
 
+function createSelectOptions(exercises, select) {
+
+    // place holder option
+    let o = document.createElement("option");
+    o.innerText = "Select exercise"
+    o.value = ""
+    select.appendChild(o);
+
+    exercises.sort();
+
+    // create from list of options
+    for (let e of exercises) {
+        o = document.createElement("option")
+        o.value = e
+        o.innerText = e
+        select.appendChild(o)
+    }
+
+}
+
+const selectLog = document.getElementById("exercise");
+const selectStats = document.getElementById("exerciseStats");
+createSelectOptions(exercises, selectLog);
+createSelectOptions(exercises, selectStats);
+
+class State {
     #exercises;
     #filter;
 
-    constructor (initExercises) {
-      this.#exercises = initExercises;
+    constructor(initExercises) {
+        this.#exercises = initExercises;
     }
 
     // arrow function declaration --> takt this from context, do not binf their own this
     addExercise = function (exercise) {
-      this.#exercises.push(exercise);
-    }    
+        this.#exercises.push(exercise);
+    };
 
     // define their own this depending on how they are called
-    setFilter (filter) {
-      this.#filter = filter;
-  }
+    setFilter(filter) {
+        this.#filter = filter;
+    }
 
     getExercises = function (filter) {
-      this.#filter = filter;
-      return this.#exercises.filter((exercise) => {
-          if (this.#filter == 'all') {
+        this.#filter = filter;
+        return this.#exercises.filter((exercise) => {
+        if (this.#filter == "all") {
             return true;
-          } else {
+        } else {
             return this.#filter == exercise.name;
-          }
-      });
-    }
+        }
+        });
+    };
 }
 
-
 const exerciseData = new State([
-  {name: "exercise1", date: new Date(), weight: 11},
-  {name: "exercise2", date: new Date(), weight: 12},
-  {name: "exercise3", date: new Date(), weight: 13},
-  {name: "exercise1", date: new Date(), weight: 15}
-])
-
+    { name: "Biceps curls", date: new Date(), weight: 11 },
+    { name: "Benchpress", date: new Date(), weight: 12 },
+    { name: "Benchpress", date: new Date(), weight: 13 },
+    { name: "Biceps curls", date: new Date(), weight: 15 },
+]);
 
 function logWorkout() {
-  var exercise = document.getElementById("exercise").value;
-  var reps = document.getElementById("reps").value;
-  var sets = document.getElementById("sets").value;
-  var weight = document.getElementById("weight").value;
+    var exercise = document.getElementById("exercise").value;
+    var reps = document.getElementById("reps").value;
+    var sets = document.getElementById("sets").value;
+    var weight = document.getElementById("weight").value;
 
-  // new row
-  var tableBody = document.getElementById("workoutTableBody");
-  var newRow = tableBody.insertRow();
+    // new row
+    var tableBody = document.getElementById("workoutTableBody");
+    var newRow = tableBody.insertRow();
 
-  // new cell for name
-  var exerciseCell = newRow.insertCell();
-  exerciseCell.textContent = exercise;
+    // new cell for name
+    var exerciseCell = newRow.insertCell();
+    exerciseCell.textContent = exercise;
 
-  // new cell for row
-  var repsCell = newRow.insertCell();
-  repsCell.textContent = reps;
+    // new cell for row
+    var repsCell = newRow.insertCell();
+    repsCell.textContent = reps;
 
-  // new cell for set
-  var setsCell = newRow.insertCell();
-  setsCell.textContent = sets;
+    // new cell for set
+    var setsCell = newRow.insertCell();
+    setsCell.textContent = sets;
 
-  // new cell for weight
-  var weightCell = newRow.insertCell();
-  weightCell.textContent = weight;
+    // new cell for weight
+    var weightCell = newRow.insertCell();
+    weightCell.textContent = weight;
 
-  let newExercise = {
-      name: exercise,
-      date: new Date(),
-      weight: parseFloat(weight)
-  }
+    let newExercise = {
+        name: exercise,
+        date: new Date(),
+        weight: parseFloat(weight),
+    };
 
-  exerciseData.addExercise(newExercise);
+    exerciseData.addExercise(newExercise);
 
-  updateChart();
+    updateChart();
 }
 
 function createWorkout() {
-  var workoutName = document.getElementById("workoutName").value;
-  var exerciseName = document.getElementById("exerciseName").value;
-  var exerciseSets = document.getElementById("exerciseSets").value;
-  var exerciseReps = document.getElementById("exerciseReps").value;
-  var exercisePR = document.getElementById("exercisePR").value;
+    var workoutName = document.getElementById("workoutName").value;
+    var exerciseName = document.getElementById("exerciseName").value;
+    var exerciseSets = document.getElementById("exerciseSets").value;
+    var exerciseReps = document.getElementById("exerciseReps").value;
+    var exercisePR = document.getElementById("exercisePR").value;
 
-  var tableBody = document.getElementById("workoutTableBody");
-  var newRow = tableBody.insertRow();
+    var tableBody = document.getElementById("workoutTableBody");
+    var newRow = tableBody.insertRow();
 
-  var workoutNameCell = newRow.insertCell();
-  workoutNameCell.textContent = workoutName;
+    var workoutNameCell = newRow.insertCell();
+    workoutNameCell.textContent = workoutName;
 
-  var exerciseNameCell = newRow.insertCell();
-  exerciseNameCell.textContent = exerciseName;
+    var exerciseNameCell = newRow.insertCell();
+    exerciseNameCell.textContent = exerciseName;
 
-  var setsCell = newRow.insertCell();
-  setsCell.textContent = exerciseSets;
+    var setsCell = newRow.insertCell();
+    setsCell.textContent = exerciseSets;
 
-  var repsCell = newRow.insertCell();
-  repsCell.textContent = exerciseReps;
+    var repsCell = newRow.insertCell();
+    repsCell.textContent = exerciseReps;
 
-  var prCell = newRow.insertCell();
-  prCell.textContent = exercisePR;
+    var prCell = newRow.insertCell();
+    prCell.textContent = exercisePR;
 }
 
 // swithc current window
 function toggleWindow(currentWindow, nextWindow) {
-  var current = document.getElementById(currentWindow);
-  var next = document.getElementById(nextWindow);
+    var current = document.getElementById(currentWindow);
+    var next = document.getElementById(nextWindow);
 
-  current.classList.remove("active-window");
-  next.classList.add("active-window");
+    current.classList.remove("active-window");
+    next.classList.add("active-window");
 }
 
 function updateChart() {
-
   // get window for charts
   const window = document.getElementById("statsWindow");
-  
+
   // get chosen exercise
   const selectedExercise = document.getElementById("exerciseStats").value;
   const data = exerciseData.getExercises(selectedExercise);
 
   // remove old one if there is
   let oldChartContainer = document.getElementById("chartContainer");
-  oldChartContainer.remove();
+  if (oldChartContainer != null) {
+    oldChartContainer.remove();
+  }
 
   // create new canvas
   let chartContainer = document.createElement("canvas");
   chartContainer.id = "chartContainer";
-  chartContainer.style.width = "1vw"
-  chartContainer.style.height = "1.2vh"
+  chartContainer.style.width = "1vw";
+  chartContainer.style.height = "1.2vh";
   window.appendChild(chartContainer);
 
   // no exercise selected
@@ -145,28 +178,28 @@ function updateChart() {
 
   // get 2d drawing context for canvas
   let ctx = chartContainer.getContext("2d");
-  
+
   // create chart
   new Chart(ctx, {
-    type: "line",
-    data: {
-      labels: labels,
-      datasets: [
-        {
-          label: "Weight (kg)",
-          data: weights,
-          borderColor: "rgba(75, 192, 192, 1)",
-          backgroundColor: "rgba(75, 192, 192, 0.2)",
-          borderWidth: 1,
+        type: "line",
+        data: {
+        labels: labels,
+        datasets: [
+            {
+            label: "Weight (kg)",
+            data: weights,
+            borderColor: "rgba(75, 192, 192, 1)",
+            backgroundColor: "rgba(75, 192, 192, 0.2)",
+            borderWidth: 1,
+            },
+        ],
         },
-      ],
-    },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true,
+        options: {
+        scales: {
+            y: {
+            beginAtZero: true,
+            },
         },
-      },
-    },
-  });
+        },
+    });
 }
